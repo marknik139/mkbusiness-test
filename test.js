8ea38b3eaaa2bca18e075aae38a0053fd8a87849
Nikita <marknik139@gmail.com>
2024-12-25 18:39:48 +0300

    format

--- test.js
+++ test.js
@@ -7,7 +7,6 @@
     5. Разместите код в гите и пришлите ссылку
 */
 
-
 /**
  * Класс для работы с API
  *
@@ -16,69 +15,70 @@
  */
 class Api
 {
-    /**
-    * Конструктор класса Api
-    *
-    * @param {Array} [api_path_templates=[]] массив шаблонов путей API, которые будут использоваться в методах класса
-    * @param {string} [baseURL=''] базовый URL, который может добавляться ко всем путям API. Например: https://mkbusiness.ru
-    */
-	constructor(api_path_templates = [], baseURL = '')
-    {
-		this.api_path_templates = api_path_templates;
-		this.baseURL = baseURL;
-	}
+  /*
+   * Конструктор класса Api
+   *
+   * @param {Array} [api_path_templates=[]] массив шаблонов путей API, которые будут использоваться в методах класса
+   * @param {string} [baseURL=''] базовый URL, который может добавляться ко всем путям API. Например: https://mkbusiness.ru
+   */
+  constructor(api_path_templates = [], baseURL = "")
+  {
+    this.api_path_templates = api_path_templates;
+    this.baseURL = baseURL;
+  }
 
-	/**
-	 * Заполняет строковый шаблон template данными из объекта object
-	 *
-	 * @author		Nikita Markelov
-	 * @version		v.1.0 (25/12/2024)
-	 * @param		{object} object
-	 * @param		{string} template
-	 * @return		{string}
-	 */
-	get_api_path(object, template)
+  /*
+   * Заполняет строковый шаблон template данными из объекта object
+   *
+   * @author    Nikita Markelov
+   * @version   v.1.0 (25/12/2024)
+   * @param     {object} object
+   * @param     {string} template
+   * @return    {string}
+   */
+  get_api_path(object, template)
+  {
+    const keyRegexpPattern = /%([a-zA-Z0-9_]+)%/g;
+    const result = template.replace(keyRegexpPattern, (_, key) =>
     {
-        const keyRegexpPattern = /%([a-zA-Z0-9_]+)%/g;
-		const result = template.replace(keyRegexpPattern, (_, key) =>
-        {
-			if (!object[key])
-            {
-				throw new Error(`Ключ "${key}" не найден в объекте`);
-			}
-			return encodeURIComponent(object[key]);
-		});
+      if (!object[key])
+      {
+        throw new Error(`Ключ "${key}" не найден в объекте`);
+      }
+      return encodeURIComponent(object[key]);
+    });
 
-		return this.baseURL + result;
-	}
+    return this.baseURL + result;
+  }
 
-	/**
-	 * Метод для получения всех путей API на основе шаблонов
-	 *
-	 * @param		{object} object
-	 * @return		{array} массив с готовыми путями
-	 */
-	get_all_api_paths(object)
+  /**
+   * Метод для получения всех путей API на основе шаблонов
+   *
+   * @param     {object} object
+   * @return    {array} массив с готовыми путями
+   */
+  get_all_api_paths(object)
+  {
+    return this.api_path_templates.map((template) =>
     {
-		return this.api_path_templates.map((template) =>
-        {
-			return this.get_api_path(object, template);
-		});
-	}
+      return this.get_api_path(object, template);
+    });
+  }
 }
 
-
-let user = {
-	id: 20,
-	name: 'John Dow',
-	role: 'QA',
-	salary: 100
+let user =
+{
+  id: 20,
+  name: "John Dow",
+  role: "QA",
+  salary: 100,
 };
 
-let api_path_templates = [
-	"/api/items/%id%/%name%",
-	"/api/items/%id%/%role%",
-	"/api/items/%id%/%salary%"
+let api_path_templates =
+[
+  "/api/items/%id%/%name%",
+  "/api/items/%id%/%role%",
+  "/api/items/%id%/%salary%",
 ];
 
 const api = new Api(api_path_templates);
@@ -87,4 +87,9 @@ const api_paths = api.get_all_api_paths(user);
 console.log(api_paths);
 
 // Ожидаемый результат
-let expected_result = ["/api/items/20/John%20Dow", "/api/items/20/QA", "/api/items/20/100"];
+let expected_result =
+[
+  "/api/items/20/John%20Dow",
+  "/api/items/20/QA",
+  "/api/items/20/100",
+];
